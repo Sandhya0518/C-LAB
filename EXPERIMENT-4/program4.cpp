5676c3b7282261516a6ed7ec7a8e9fddca3e04d0
Sandhya0518 <93859004+Sandhya0518@users.noreply.github.com>
2022-02-07 21:14:13 +0530

    Add files via upload

---
+++ EXPERIMENT-4/program4.cpp
@@ -0,0 +1,27 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+#include <iostream>
+#include <string>
+using namespace std;
+struct Student{
+	string name;
+	int rollno;
+	int marks;
+}s1;
+void display(){
+	cout << s1.name << endl;
+	cout << s1.rollno << endl;
+	cout << s1.marks << endl;
+}
+int main(){
+	cout << "Enter name: ";
+	cin >> s1.name;
+	cout << "Enter rollno";
+	cin >>  s1.rollno;
+	cout << "Enter marks: ";
+	cin >> s1.marks;
+	display();
+}
+
+
